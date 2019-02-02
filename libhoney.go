6f3740223aa7af6475a261ea781ea746c51378d8
Ian Wilkes <ian.wilkes@gmail.com>
2019-02-01 16:43:58 -0800

    update version number

--- libhoney.go
+++ libhoney.go
@@ -30,7 +30,7 @@ func init() {
 const (
 	defaultSampleRate = 1
 	defaultAPIHost    = "https://api.honeycomb.io/"
-	version           = "1.8.1"
+	version           = "1.8.2"
 
 	// DefaultMaxBatchSize how many events to collect in a batch
 	DefaultMaxBatchSize = 50
