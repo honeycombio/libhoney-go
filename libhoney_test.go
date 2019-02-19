90121c0293955f562d1e3efaca42c45f96ca731b
Ben Hartshorne <ben@honeycomb.io>
2019-02-19 14:17:39 -0800

    re-add missing mock output to match old api surface

--- libhoney_test.go
+++ libhoney_test.go
@@ -524,44 +524,6 @@ func TestBuilderStaticFields(t *testing.T) {
 	testEquals(t, ev4.data["floatF"], nil)
 }
 
-// MockOutput implements the Output interface by retaining a slice of added
-// events, for use in unit tests. Deprecated, should use the transmission.Sender
-// interface instead.
-type MockOutput struct {
-	Started          int
-	Stopped          int
-	EventsCalled     int
-	events           []*Event
-	responses        chan Response
-	BlockOnResponses bool
-	sync.Mutex
-}
-
-func (m *MockOutput) Add(ev *Event) {
-	m.Lock()
-	m.events = append(m.events, ev)
-	m.Unlock()
-}
-
-func (m *MockOutput) Start() error {
-	m.Started += 1
-	m.responses = make(chan Response, 1)
-	return nil
-}
-func (m *MockOutput) Stop() error {
-	m.Stopped += 1
-	return nil
-}
-
-func (m *MockOutput) Events() []*Event {
-	m.EventsCalled += 1
-	m.Lock()
-	defer m.Unlock()
-	output := make([]*Event, len(m.events))
-	copy(output, m.events)
-	return output
-}
-
 func TestOutputInterface(t *testing.T) {
 	resetPackageVars()
 	testTx := &MockOutput{}
