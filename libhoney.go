d3dd0446a5689ccbcdd5a8a6e3d18916d01870ad
Mike Goldsmith <goldsmith.mike@gmail.com>
2021-11-03 13:50:18 +0000

    prepare v1.15.6 release (#152)

--- libhoney.go
+++ libhoney.go
@@ -33,7 +33,7 @@ const (
 	defaultSampleRate = 1
 	defaultAPIHost    = "https://api.honeycomb.io/"
 	defaultDataset    = "libhoney-go dataset"
-	version           = "1.15.5"
+	version           = "1.15.6"
 
 	// DefaultMaxBatchSize how many events to collect in a batch
 	DefaultMaxBatchSize = 50
